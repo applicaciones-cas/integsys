7a7c98be7b9deb40b00b42ac430231e75c4caff8
Aldristh456034 <trinidadaldrich328@gmail.com>
2026-01-07 14:05:22 +0800

    01072026 - SOA controller cleanup

--- src/main/java/ph/com/guanzongroup/integsys/views/SOATagging_HistoryCarController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/SOATagging_HistoryCarController.java
@@ -15,7 +15,6 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.beans.property.ReadOnlyBooleanPropertyBase;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -49,7 +48,6 @@ import org.guanzon.appdriver.base.GuanzonException;
 import org.guanzon.appdriver.base.MiscUtil;
 import org.guanzon.appdriver.constant.EditMode;
 import org.json.simple.JSONObject;
-import ph.com.guanzongroup.cas.cashflow.SOATagging;
 import ph.com.guanzongroup.cas.cashflow.services.CashflowControllers;
 import ph.com.guanzongroup.cas.cashflow.status.SOATaggingStatic;
 
