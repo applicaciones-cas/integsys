240b37dc3873ff50064dad7d87638cfe356e6abe
Aldristh456034 <trinidadaldrich328@gmail.com>
2026-01-10 16:26:54 +0800

    01102026 - DV fxml and controller

--- src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_ViewController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_ViewController.java
@@ -103,7 +103,7 @@ public class DisbursementVoucher_ViewController implements Initializable, Screen
     @FXML
     private StackPane StackPane;
     @FXML
-    private Button btnClose;
+    private Button btnHistory, btnClose;
     @FXML
     private TabPane tabPaneMain, tabPanePaymentMode;
     @FXML
@@ -279,7 +279,7 @@ public class DisbursementVoucher_ViewController implements Initializable, Screen
     }
 
     private void initButtonsClickActions() {
-        List<Button> buttons = Arrays.asList(btnClose);
+        List<Button> buttons = Arrays.asList(btnHistory, btnClose);
         buttons.forEach(button -> button.setOnAction(this::cmdButton_Click));
     }
 
@@ -288,14 +288,14 @@ public class DisbursementVoucher_ViewController implements Initializable, Screen
         String lsButton = ((Button) event.getSource()).getId();
         switch (lsButton) {
             case "btnHistory":
-                if(pnEditMode != EditMode.READY && pnEditMode != EditMode.UPDATE){
+                if (pnEditMode != EditMode.READY && pnEditMode != EditMode.UPDATE) {
                     ShowMessageFX.Warning("No transaction status history to load!", pxeModuleName, null);
                     return;
-                } 
+                }
 
                 try {
                     poController.ShowStatusHistory();
-                }  catch (NullPointerException npe) {
+                } catch (NullPointerException npe) {
                     Logger.getLogger(getClass().getName()).log(Level.SEVERE, MiscUtil.getException(npe), npe);
                     ShowMessageFX.Error("No transaction status history to load!", pxeModuleName, null);
                 } catch (Exception ex) {
