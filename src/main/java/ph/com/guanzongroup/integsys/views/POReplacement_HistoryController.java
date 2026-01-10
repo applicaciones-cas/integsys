8a865ee8ee1f4ea1b34e8fe40d5f7481407d6c94
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-01-10 14:12:51 +0800

    01102026 - Update PO Replacement Entry, Confirmation and History
     - Update PO Replacement Entry, Confirmation and History

--- src/main/java/ph/com/guanzongroup/integsys/views/POReplacement_HistoryController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/POReplacement_HistoryController.java
@@ -252,6 +252,20 @@ public class POReplacement_HistoryController implements Initializable, ScreenInt
                         showSerialDialog();
                         return;
                     case "btnHistory":
+                        if(pnEditMode != EditMode.READY && pnEditMode != EditMode.UPDATE){
+                            ShowMessageFX.Warning("No transaction status history to load!", pxeModuleName, null);
+                            return;
+                        } 
+                        
+                        try {
+                            poController.PurchaseOrderReceiving().ShowStatusHistory();
+                        }  catch (NullPointerException npe) {
+                            Logger.getLogger(getClass().getName()).log(Level.SEVERE, MiscUtil.getException(npe), npe);
+                            ShowMessageFX.Error("No transaction status history to load!", pxeModuleName, null);
+                        } catch (Exception ex) {
+                            Logger.getLogger(getClass().getName()).log(Level.SEVERE, MiscUtil.getException(ex), ex);
+                            ShowMessageFX.Error(MiscUtil.getException(ex), pxeModuleName, null);
+                        }
                         break;
 
                     case "btnArrowRight":
