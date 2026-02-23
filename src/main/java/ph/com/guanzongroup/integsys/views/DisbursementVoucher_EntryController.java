9c0706e015dbc4c9db2f32e2479b8cd2a0057698
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-02-23 14:11:04 +0800

    02232026 - Update Disbursement Voucher Entry
    - Added update edit mode validation in btnSave

--- src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_EntryController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_EntryController.java
@@ -442,10 +442,12 @@ public class DisbursementVoucher_EntryController implements Initializable, Scree
                     break;
                 case "btnSave":
                     //Recheck transaction status
-                    poJSON = poController.checkUpdateTransaction(true);
-                    if (!"success".equals((String) poJSON.get("result"))) {
-                        ShowMessageFX.Warning(null, pxeModuleName, (String) poJSON.get("message"));
-                        return;
+                    if(pnEditMode == EditMode.UPDATE){
+                        poJSON = poController.checkUpdateTransaction(true);
+                        if (!"success".equals((String) poJSON.get("result"))) {
+                            ShowMessageFX.Warning(null, pxeModuleName, (String) poJSON.get("message"));
+                            return;
+                        }
                     }
 
                     if (!ShowMessageFX.YesNo(null, pxeModuleName, "Are you sure you want to save the transaction?")) {
