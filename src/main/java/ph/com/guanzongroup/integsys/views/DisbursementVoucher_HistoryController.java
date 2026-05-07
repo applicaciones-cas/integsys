c81e79fccf6c5d109bf92a42363406038396bc6c
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-05-07 11:12:49 +0800

    05072026 - Update Disbursement Voucher History
    - Added functionality for loading of transaction source view

--- src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_HistoryController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_HistoryController.java
@@ -89,11 +89,12 @@ public class DisbursementVoucher_HistoryController implements Initializable, Scr
 
     private GRiderCAS oApp;
     private JSONObject poJSON;
+    JFXUtil.StageManager stageView = new JFXUtil.StageManager();
+    private final String pxeModuleName = "Disbursement Voucher History";
     private int pnDetail = 0;
     private int pnDetailJE = 0;
     private int pnDetailBIR = 0;
     private int pnAttachment = 0;
-    private final String pxeModuleName = "Disbursement Voucher History";
     private DisbursementVoucher poController;
     public int pnEditMode;
     private String psIndustryId = "";
@@ -919,12 +920,23 @@ public class DisbursementVoucher_HistoryController implements Initializable, Scr
             }
         });
         tblVwDetails.setOnMouseClicked(event -> {
-            if (!details_data.isEmpty() && event.getClickCount() == 1) {
+            if (!details_data.isEmpty()) {
                 ModelDisbursementVoucher_Detail selected = (ModelDisbursementVoucher_Detail) tblVwDetails.getSelectionModel().getSelectedItem();
-                if (selected != null) {
-                    int lnRow = Integer.parseInt(details_data.get(tblVwDetails.getSelectionModel().getSelectedIndex()).getIndex11());
-                    pnDetail = lnRow;
-                    loadRecordDetail();
+                switch(event.getClickCount()){
+                    case 1:
+                        if (selected != null) {
+                            int lnRow = Integer.parseInt(details_data.get(tblVwDetails.getSelectionModel().getSelectedIndex()).getIndex11());
+                            pnDetail = lnRow;
+                            loadRecordDetail();
+                        }
+                    break;
+                    case 2:
+                        if (selected != null) {
+                            int lnRow = Integer.parseInt(details_data.get(tblVwDetails.getSelectionModel().getSelectedIndex()).getIndex11());
+                            pnDetail = lnRow;
+                            loadDetailView();
+                        }
+                    break;
                 }
             }
         });
@@ -945,6 +957,66 @@ public class DisbursementVoucher_HistoryController implements Initializable, Scr
         });
         JFXUtil.setKeyEventFilter(this::tableKeyEvents, tblVwDetails, tblVwJournalDetails, tblAttachments);
         JFXUtil.adjustColumnForScrollbar(tblVwDetails, tblVwJournalDetails, tblAttachments);
+    }
+    
+    private void loadDetailView() {
+        try {
+            String lsSourceCode = poController.Detail(pnDetail).getSourceCode();
+            String lsSourceNo = poController.Detail(pnDetail).getSourceNo();
+            
+            if(lsSourceCode == null || "".equals(lsSourceCode)){
+                return;
+            }
+            
+            if (DisbursementStatic.SourceCode.ACCOUNTS_PAYABLE.equals(lsSourceCode) ) {
+                lsSourceCode = poController.Detail(pnDetail).SOADetail().getSourceCode();
+                lsSourceNo = poController.Detail(pnDetail).SOADetail().getSourceNo();
+            }
+            loadBySourceCode(lsSourceCode, lsSourceNo);
+        } catch (SQLException | GuanzonException ex) {
+            Logger.getLogger(getClass().getName())
+                  .log(Level.SEVERE, MiscUtil.getException(ex), ex);
+            ShowMessageFX.Error(null, pxeModuleName, MiscUtil.getException(ex));
+        }
+    }
+
+    private void loadBySourceCode(String fsSourceCode, String fsSourceNo) {
+        poJSON = new JSONObject();
+        stageView.closeDialog();
+        
+        switch (fsSourceCode) {
+            case DisbursementStatic.SourceCode.PAYMENT_REQUEST:
+                PaymentRequest_ViewController loPRFController = new PaymentRequest_ViewController();
+                loPRFController.setGRider(oApp);
+                loPRFController.setTransaction(fsSourceNo);
+                showDialog("/ph/com/guanzongroup/integsys/views/PaymentRequest_View.fxml", loPRFController);
+                break;
+            case DisbursementStatic.SourceCode.AP_ADJUSTMENT:
+                APPaymentAdjustment_ViewController loAPAdjController = new APPaymentAdjustment_ViewController();
+                loAPAdjController.setGRider(oApp);
+                loAPAdjController.setTransaction(fsSourceNo);
+                showDialog("/ph/com/guanzongroup/integsys/views/APPaymentAdjustment_View.fxml", loAPAdjController);
+                break;
+            case DisbursementStatic.SourceCode.PO_RECEIVING:
+                SIPosting_ViewController loSIPostingController = new SIPosting_ViewController();
+                loSIPostingController.setGRider(oApp);
+                loSIPostingController.setTransaction(fsSourceNo);
+                showDialog("/ph/com/guanzongroup/integsys/views/SIPosting_View.fxml", loSIPostingController);
+                break;
+            default:
+                ShowMessageFX.Warning(null, pxeModuleName, "Failed to open detail form for source: " + fsSourceCode + ". Please contact the system administrator.");
+                break;
+        }
+    }
+    
+    private void showDialog(String fsSource, Object controller ){
+        try {
+            stageView.showDialog((Stage) AnchorMain.getScene().getWindow(), getClass().getResource(fsSource), controller,
+                    "Disbursement Dialog", true, true, false);
+        } catch (IOException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+            ShowMessageFX.Error(null, pxeModuleName, MiscUtil.getException(ex));
+        }
     }
 
     private void tableKeyEvents(KeyEvent event) {
