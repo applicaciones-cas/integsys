3f1cfa8466e39c7c2ed1a3e3a7416c4b8581bf20
Arsiela <arsiela68@gmail.com>
2026-06-08 10:55:46 +0800

    06082026 - Update Check Authorization and DV Certification
    - Added check deposit in payment form

--- src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_CertificationController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/DisbursementVoucher_CertificationController.java
@@ -373,8 +373,12 @@ public class DisbursementVoucher_CertificationController implements Initializabl
 
                                     switch (disbursementType) {
                                         case DisbursementStatic.DisbursementType.CHECK:
-                                        case DisbursementStatic.DisbursementType.CHECK_DEPOSIT:
                                             lsPaymentForm = "CHECK";
+                                            lsBankName = poDisbursementController.getMaster(lnCntr).CheckPayments().Banks().getBankName();
+                                            lsBankAccount = poDisbursementController.getMaster(lnCntr).CheckPayments().Bank_Account_Master().getAccountNo();
+                                            break;
+                                        case DisbursementStatic.DisbursementType.CHECK_DEPOSIT:
+                                            lsPaymentForm = "CHECK DEPOSIT";
                                             lsBankName = poDisbursementController.getMaster(lnCntr).CheckPayments().Banks().getBankName();
                                             lsBankAccount = poDisbursementController.getMaster(lnCntr).CheckPayments().Bank_Account_Master().getAccountNo();
                                             break;
