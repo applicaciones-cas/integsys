a18954b90437b60f7adbf8872c663cff286d7140
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-07-01 09:57:22 +0800

    07012026 - Update SI Posting Verification

--- src/main/java/ph/com/guanzongroup/integsys/views/SIPosting_VerificationMCController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/SIPosting_VerificationMCController.java
@@ -569,6 +569,7 @@ public class SIPosting_VerificationMCController implements Initializable, Screen
                                 JSONObject loJSON = poPurchaseReceivingController.PurchaseOrderReceiving().OpenTransaction(poPurchaseReceivingController.PurchaseOrderReceiving().Master().getTransactionNo());
                                 if ("success".equals(loJSON.get("result"))) {
                                     if (poPurchaseReceivingController.PurchaseOrderReceiving().Master().getTransactionStatus().equals(PurchaseOrderReceivingStatus.CONFIRMED_I)) {
+                                        populateJE();
                                         if (ShowMessageFX.YesNo(null, pxeModuleName, "Do you want to verify this transaction?")) {
                                             loJSON = poPurchaseReceivingController.PurchaseOrderReceiving().VerifySIPosting("");
                                             if ("success".equals((String) loJSON.get("result"))) {
